34e4617ff0d6fc1f6c42bde8824eb7e597aaf3a5
Rijn <bxbian951122@gmail.com>
2017-07-16 01:07:49 -0500

    add usd

--- server/plugins/pool/render.js
+++ server/plugins/pool/render.js
@@ -4,26 +4,39 @@ export default {
   template: `<div>
     <p>EARNED : {{ earned }} ZEC</p>
     <p>UNPAID : {{ unpaid }} ZEC</p>
+    <p>$ {{ usd }}</p>
   </div>`,
 
   data () {
     return {
       earned: 'pending',
-      unpaid: 'pending'
+      unpaid: 'pending',
+      ZECUSD: 'pending'
+    }
+  },
+
+  computed: {
+    usd: function () {
+      return ((Number(this.earned) + Number(this.unpaid)) * Number(this.ZECUSD)).toFixed(2);
     }
   },
 
   created () {
     this.$bus.on(this.$options.name, this.process)
+    this.$bus.on('zcash-usd', this.exchange)
   },
   beforeDestroy () {
     this.$bus.off(this.$options.name, this.process)
+    this.$bus.off('zcash-usd', this.exchange)
   },
 
   methods: {
     process ({ earned, unpaid }) {
       this.earned = earned;
       this.unpaid = unpaid;
+    },
+    exchange ({ ZECUSD }) {
+      this.ZECUSD = ZECUSD;
     }
   }
 }
